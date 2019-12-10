517cfcd06358c073dc0484d40f2451146ac1aa11
Jarrett <jarrettphale@gmail.com>
2019-12-09 20:40:23 -0500

    Leetcode challenge 1281

--- Leetcode/subtractproductsum.js
+++ Leetcode/subtractproductsum.js
@@ -1,7 +1,15 @@
 const num = 678
 
 const subtractProductAndSum = function(n) {
-    
+  let deconstructed_numbers = n.toString().split('').map(Number)
+  let product = 1,
+      sum = 0
+  deconstructed_numbers.forEach((number, index) => {
+    sum += deconstructed_numbers[index];
+    product *= deconstructed_numbers[index];
+  })
+  difference_of_product_and_sum = product - sum
+  return difference_of_product_and_sum
 };
 
 subtractProductAndSum(num)
