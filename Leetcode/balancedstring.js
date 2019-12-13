7e0e3c0538c98364d2d0fa586200fbf622636aa6
Jarrett <jarrettphale@gmail.com>
2019-12-12 23:00:51 -0500

    1221 Balanced string solution

---
+++ Leetcode/balancedstring.js
@@ -0,0 +1,21 @@
+const s = "RLRRLLRLRL";
+
+const balancedStringSplit = function(s) {
+	let count = 0;
+	let temp = 1;
+	for (let char of s) {
+		if (char === "R") {
+			temp++;
+		} else {
+			temp--;
+		}
+
+		if (temp === 1) {
+			count++;
+		}
+  }
+  console.log(count)
+	return count;
+};
+
+balancedStringSplit(s);
